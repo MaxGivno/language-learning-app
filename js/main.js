d8353493251df43f68ac5081d196b8e5d6738e63
Max Givno <max.ryazanov@gmail.com>
2020-09-22 17:11:30 +0300

    Language select menu interaction. Card started

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ const lngBtn = document.querySelector('.lng-select')
 const menu = document.querySelector('.lng-menu')
 const lngMenuItems = document.querySelectorAll('.lng-menu-item')
 const contentItems = document.querySelectorAll('.content-grid')
+const contentItemCard = document.getElementById('clickable')
+const closeBtn = document.querySelector('.close-btn')
 let lastSelected = 'usa'
 
 function toggleMenu(e) {
@@ -14,8 +16,8 @@ function toggleMenu(e) {
 
 function clearSelected() {
     lngBtn.classList.remove(lastSelected)
+
     lngMenuItems.forEach(item => item.classList.remove('selected'))
-    // console.log(lastSelected)
 }
 
 function hideContent() {
@@ -30,9 +32,22 @@ function switchContent(e) {
     this.classList.add('selected')
     const contentId = `#${this.id}-content`
     const content = document.querySelector(contentId)
-    content.classList.add('show')
+    content && content.classList.add('show')
     lastSelected = this.id
+}
+
+function expandCard(e) {
+    console.log(this.id)
+    this.classList.add('expanded')
+    contentItemCard.removeEventListener('click', expandCard)
+}
+
+async function closeCard(e) {
+    console.log(this.id)
+    contentItemCard.classList.remove('expanded')
 }
 
 lngBtn.addEventListener('click', toggleMenu);
 lngMenuItems.forEach(item => item.addEventListener('click', switchContent))
+contentItemCard.addEventListener('click', expandCard)
+closeBtn.addEventListener('click', closeCard)
